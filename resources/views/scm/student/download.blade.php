0e4f10e40e3573f754dc941458b088c5f267db98
Kevin Alama <alama1992@gmail.com>
2020-05-31 00:10:29 -0500

    views finished

--- resources/views/scm/student/download.blade.php
+++ resources/views/scm/student/download.blade.php
@@ -4,17 +4,21 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <h3>Subir una lista de alumnos</h3>        
+            <h3>Descarga de formatos</h3>        
             <br><br>  
             <form method="POST" action="{{ route('classrooms.exports.students', ['classroom_id' => $classroom_id]) }}">
-                @csrf                
-                <select>  
-                    <option>Selecciona un formato deseado </option>
-                    <option>Formato fecha </option>
-                </select>
-
+                @csrf                                
+                <div class="form-group">
+                    <label for="file-formats">Selecciona un formato deseado</label>
+                    <select class="form-control" id="file-formats">
+                    <option>Firmas de padres</option>
+                    <option>Asistencia por mes</option>
+                    <option>Calificaciones</option>
+                    </select>
+                </div>
                 <br><br>  
-                <button type="submit" class="btn btn-primary">Generar formato</button>                
+                <button type="submit" class="btn btn-success">Generar formato</button> 
+                <a class="btn btn-secondary" href="{{ route('classrooms.students', ['classroom_id' => $classroom_id]) }}" role="button">Regresar a listado</a>               
             </form>
         </div>       
     </div>
